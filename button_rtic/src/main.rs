cbf40b49b7a2256833218a36942877f65e33466f
Riccardo Casatta <riccardo@casatta.it>
2021-06-18 23:21:27 +0200

    two buttons with 3 functions logic

--- button_rtic/src/main.rs
+++ button_rtic/src/main.rs
@@ -13,15 +13,28 @@ use panic_halt as _;
 
 use rtic::app;
 
-use stm32f1xx_hal::gpio::{gpioa::PA7, gpioc::PC13, Edge};
+use embedded_hal::digital::v2::{InputPin, OutputPin};
+use stm32f1xx_hal::gpio::gpioa::{PA0, PA1, PA2, PA5, PA6, PA7};
+use stm32f1xx_hal::gpio::{gpioc::PC13, Edge};
 use stm32f1xx_hal::gpio::{ExtiPin, Floating, Input, Output, PushPull, State};
 use stm32f1xx_hal::prelude::*;
+
+pub struct Button<T> {
+    pin: T,
+    pressed: bool,
+}
 
 #[app(device = stm32f1xx_hal::pac, peripherals = true)]
 const APP: () = {
     struct Resources {
         led: PC13<Output<PushPull>>,
-        pin: PA7<Input<Floating>>,
+
+        led0: PA0<Output<PushPull>>,
+        led1: PA1<Output<PushPull>>,
+        led2: PA2<Output<PushPull>>,
+
+        pa5: Button<PA5<Input<Floating>>>,
+        pa6: Button<PA6<Input<Floating>>>,
     }
 
     #[init]
@@ -37,12 +50,46 @@ const APP: () = {
 
         let mut gpioa = cx.device.GPIOA.split(&mut rcc.apb2);
 
-        let mut pin = gpioa.pa7.into_floating_input(&mut gpioa.crl);
-        pin.make_interrupt_source(&mut afio);
-        pin.trigger_on_edge(&cx.device.EXTI, Edge::RISING_FALLING);
-        pin.enable_interrupt(&cx.device.EXTI);
+        // Setup Leds
+        let led0 = gpioa
+            .pa0
+            .into_push_pull_output_with_state(&mut gpioa.crl, State::Low);
+        let led1 = gpioa
+            .pa1
+            .into_push_pull_output_with_state(&mut gpioa.crl, State::Low);
+        let led2 = gpioa
+            .pa2
+            .into_push_pull_output_with_state(&mut gpioa.crl, State::Low);
 
-        init::LateResources { led, pin }
+        // Setup Buttons
+        let mut pa5 = gpioa.pa5.into_floating_input(&mut gpioa.crl);
+        pa5.make_interrupt_source(&mut afio);
+        pa5.trigger_on_edge(&cx.device.EXTI, Edge::RISING_FALLING);
+        pa5.enable_interrupt(&cx.device.EXTI);
+        let pa5 = Button {
+            pin: pa5,
+            pressed: false,
+        };
+
+        let mut pa6 = gpioa.pa6.into_floating_input(&mut gpioa.crl);
+        pa6.make_interrupt_source(&mut afio);
+        pa6.trigger_on_edge(&cx.device.EXTI, Edge::RISING_FALLING);
+        pa6.enable_interrupt(&cx.device.EXTI);
+        let pa6 = Button {
+            pin: pa6,
+            pressed: false,
+        };
+
+        // Setup Leds
+
+        init::LateResources {
+            led,
+            led0,
+            led1,
+            led2,
+            pa5,
+            pa6,
+        }
     }
 
     #[idle]
@@ -52,9 +99,37 @@ const APP: () = {
         }
     }
 
-    #[task(binds = EXTI9_5, priority = 1, resources = [led, pin])]
+    #[task(binds = EXTI9_5, priority = 1, resources = [led, led0, led1, led2, pa5, pa6])]
     fn pin(cx: pin::Context) {
-        cx.resources.led.toggle().unwrap();
-        cx.resources.pin.clear_interrupt_pending_bit();
+        if !cx.resources.pa5.pressed && cx.resources.pa5.pin.is_low().unwrap() {
+            // PA5 RISING
+            cx.resources.pa5.pressed = true;
+        } else if cx.resources.pa5.pressed && cx.resources.pa5.pin.is_high().unwrap() {
+            // PA5 FALLING
+            cx.resources.pa5.pressed = false;
+            if cx.resources.pa6.pressed {
+                // BOTH pressed
+                cx.resources.pa6.pressed = false;
+                cx.resources.led0.toggle().unwrap();
+            } else {
+                cx.resources.led1.toggle().unwrap();
+            }
+        } else if !cx.resources.pa6.pressed && cx.resources.pa6.pin.is_low().unwrap() {
+            // PA6 RISING
+            cx.resources.pa6.pressed = true;
+        } else if cx.resources.pa6.pressed && cx.resources.pa6.pin.is_high().unwrap() {
+            // PA6 FALLING
+            cx.resources.pa6.pressed = false;
+            if cx.resources.pa5.pressed {
+                // BOTH pressed
+                cx.resources.pa5.pressed = false;
+                cx.resources.led0.toggle().unwrap();
+            } else {
+                cx.resources.led2.toggle().unwrap();
+            }
+        }
+
+        cx.resources.pa5.pin.clear_interrupt_pending_bit();
+        cx.resources.pa6.pin.clear_interrupt_pending_bit();
     }
 };
